04bf8c951bd08db3a0fb69a45516a9af200599fe
Charlie Whitfield <charliewhitfield@zoho.com>
2019-11-02 10:23:40 -0400

    added licence header

--- asteroid_importer.gd
+++ asteroid_importer.gd
@@ -1,9 +1,29 @@
 # asteroid_importer.gd
-# I, Voyager
-# Copyright (c) 2017-2019 Charlie Whitfield
+# This file is part of I, Voyager
+# https://ivoyager.dev
 # *****************************************************************************
-# Converts source file data to binaries used at runtime. See README.md for
-# info.
+# Copyright (c) 2017-2019 Charlie Whitfield
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# *****************************************************************************
+#
+# Converts source file data to binaries used at runtime. See README.md.
 
 extends PopupPanel
 const SCENE := "res://asteroid_importer/asteroid_importer.tscn"
